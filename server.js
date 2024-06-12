b7793d3f3fce455ec5d811ad6c1bd18dad61035a
Husky Lover <ssolivares0@gmail.com>
2024-06-11 23:42:59 -0400

    Deleted unnecessary config folder, added connection to mongoose in server.js file instead

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // import required packages 
 const express = require('express');
-const db = require('./config/connection');
+const mongoose = require('mongoose');
 const routes = require('./routes'); 
 
 // initialize the app with express
@@ -11,5 +11,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 app.use(routes); 
+
+// connect to mongoose database
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017//social-network', {
+    useFindAndModify: false,
+    useNewURLParser: true,
+    useUnifiedTopology: true
+});
+// enable mongo query logging 
+mongoose.set('debug', true);
 
 app.listen(PORT, ()=> console.log(`Connected on localhost:${PORT}`));
